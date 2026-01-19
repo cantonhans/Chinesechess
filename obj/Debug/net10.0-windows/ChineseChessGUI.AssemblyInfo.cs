252830bba0c6d63f27590367e11aabbccdfd58a3
cantonhans <muncdy@gmail.com>
2026-01-19 17:47:39 +1300

    readme update

--- obj/Debug/net10.0-windows/ChineseChessGUI.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/ChineseChessGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChineseChessGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f3ae44c602fd9e9def8b21db18b630459c72ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChineseChessGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChineseChessGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
